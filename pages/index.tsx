7b83077eeb78e413ae90d8a21e86cf6734695d4a
ACMCMC <20495460+ACMCMC@users.noreply.github.com>
2023-02-24 15:38:11 +0100

    changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,16 @@ import ArrowIcon from '../components/ArrowIcon';
 import { getGlobalData } from '../utils/global-data';
 import SEO from '../components/SEO';
 
-export default function Index({ posts, globalData }) {
+import { parseProperties, Post } from '../api/parse-properties';
+import { queryDatabase } from '../api/query-database';
+import { any } from 'cypress/types/bluebird';
+
+type IndexProps = {
+  posts: Post[];
+  globalData: any;
+};
+
+export default function Index({ posts, globalData }: IndexProps) {
   return (
     <Layout>
       <SEO title={globalData.name} description={globalData.blogTitle} />
@@ -19,6 +28,54 @@ export default function Index({ posts, globalData }) {
           {globalData.blogTitle}
         </h1>
         */}
+        <ul className="w-full">
+          {posts.map((post) => (
+            <li
+              key={post.id}
+              className="md:first:rounded-t-lg md:last:rounded-b-lg backdrop-blur-lg bg-white dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 border-b-0 last:border-b hover:border-b hovered-sibling:border-t-0"
+            >
+              <Link
+                as={`/posts/${post.id.replace(/\.mdx?$/, '')}`}
+                href={`/posts/[slug]`}
+              >
+                <a className="py-6 lg:py-10 px-6 lg:px-16 block focus:outline-none focus:ring-4">
+                  {post.data.date && (
+                    <p className="uppercase mb-3 font-bold opacity-60">
+                      {post.data.date}
+                    </p>
+                  )}
+                  <h2 className="text-2xl md:text-3xl">{post.data.title}</h2>
+                  {post.data.description && (
+                    <p className="mt-3 text-lg opacity-60">
+                      {post.data.description}
+                    </p>
+                  )}
+                  <ArrowIcon className="mt-4" />
+                </a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </main>
+      <Footer copyrightText={globalData.footerText} />
+      <GradientBackground
+        variant="large"
+        className="fixed top-20 opacity-40 dark:opacity-60"
+      />
+      <GradientBackground
+        variant="small"
+        className="absolute bottom-0 opacity-20 dark:opacity-10"
+      />
+    </Layout>
+  );
+
+
+  {/*
+  return (
+    <Layout>
+      <SEO title={globalData.name} description={globalData.blogTitle} />
+      <Header name={globalData.name} />
+      <main className="w-full">
         <ul className="w-full">
           {posts.map((post) => (
             <li
@@ -59,11 +116,5 @@ export default function Index({ posts, globalData }) {
       />
     </Layout>
   );
-}
-
-export function getStaticProps() {
-  const posts = getPosts();
-  const globalData = getGlobalData();
-
-  return { props: { posts, globalData } };
+  */}
 }
