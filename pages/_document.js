43c342123e0534f544ae7f9f55822631e2892811
ACMCMC <20495460+ACMCMC@users.noreply.github.com>
2023-02-24 23:38:36 +0100

    favicon

--- pages/_document.js
+++ pages/_document.js
@@ -4,8 +4,10 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en" className="theme-compiled">
-        <Head />
+      <Html lang="es" className="theme-compiled">
+        <Head>
+          <link rel="shortcut icon" href="/favicon.ico" />
+        </Head>
         <body
           className={`antialiased text-lg bg-white dark:bg-gray-900 dark:text-white leading-base`}
         >
