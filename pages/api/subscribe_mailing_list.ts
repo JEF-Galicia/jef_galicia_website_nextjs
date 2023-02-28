f263df613b15656cb5cc8cf04bc036934061a8cc
ACMCMC <20495460+ACMCMC@users.noreply.github.com>
2023-02-28 20:33:32 +0100

    msg

--- pages/api/subscribe_mailing_list.ts
+++ pages/api/subscribe_mailing_list.ts
@@ -9,7 +9,7 @@ export default async (req, res) => {
   const { email } = req.body;
  
   if (!email) {
-    return res.status(400).json({ error: 'Email is required' });
+    return res.status(400).json({ error: 'Introduce o teu email' });
   }
  
   try {
