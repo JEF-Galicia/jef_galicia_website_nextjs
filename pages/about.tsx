e0c3eefc323b9bab0440101a326288671b4213e0
ACMCMC <20495460+ACMCMC@users.noreply.github.com>
2023-03-08 19:41:53 +0100

    style

--- pages/about.tsx
+++ pages/about.tsx
@@ -39,20 +39,24 @@ export default function Index({ users }: IndexProps) {
                     <li
                         key={user.id}
                         className="md:first:rounded-t-lg md:last:rounded-b-lg backdrop-blur-lg bg-white dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 border-b-0 last:border-b hover:border-b hovered-sibling:border-t-0">
-                        <div className="py-6 lg:py-10 px-6 lg:px-16 block focus:outline-none focus:ring-4">
-                            <h1>
+                        <div className="py-6 lg:py-10 px-6 lg:px-16 focus:outline-none focus:ring-4 block md:flex gap-8 items-center">
+                            <div className="mb-4 md:mb-0 md:basis-1/6 md:text-right">
                                 {user.avatar_url && (
-                                    <Image src={user.avatar_url} alt="avatar" width={32} height={32} className="rounded inline-block mr-3"></Image>
+                                    <Image src={user.avatar_url} alt="avatar" width={64} height={64} className="rounded inline-block"></Image>
                                 )}
-                                {user.name}
-                            </h1>
-                            {
-                                user['person'] && user['person']['email'] && (
-                                    <p className="mt-3 text-lg opacity-60">
-                                        {user['person']['email']}
-                                    </p>
-                                )
-                            }
+                            </div>
+                            <div className='grow'>
+                                <h2>
+                                    {user.name}
+                                </h2>
+                                {
+                                    user['person'] && user['person']['email'] && (
+                                        <a href={'mailto:'+user['person']['email']} className="mt-3 text-lg opacity-60">
+                                            {user['person']['email']}
+                                        </a>
+                                    )
+                                }
+                            </div>
                         </div>
                     </li>
                 ))}
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -4,7 +4,7 @@ export default function Contact() {
     return (
         <BoxComponent>
             <h1 className='text-3xl font-semibold mb-6'>Contacta con nós</h1>
-            <p className='mb-6'>Se queres contactar con nós, podes facelo a través das nosas redes sociais, ou enviando un mail a <a href="mailto:info@jef.gal">info@jef.gal</a>.</p>
+            <p className='mb-6'>Se queres contactar con nós, podes facelo a través das nosas redes sociais, ou enviando un mail a <a href="mailto:info@jef.gal" className=" opacity-60">info@jef.gal</a>.</p>
             <p>Esperámoste! 🙌</p>
         </BoxComponent>
     )
