cd73e78400bde598cbe31ed80f632d3d9f0442b8
ACMCMC <20495460+ACMCMC@users.noreply.github.com>
2023-05-08 02:32:55 +0200

    SEO

--- next-seo.config.ts
+++ next-seo.config.ts
@@ -2,8 +2,9 @@ import { DefaultSeoProps } from 'next-seo';
 
 const config: DefaultSeoProps = {
   titleTemplate: '%s | JEF Galicia',
-  defaultTitle: 'JEF Galicia',
+  defaultTitle: 'JEF Galicia - Xeración Europea Federalista Galicia',
   canonical: 'https://jef.gal/',
+  description: 'JEF Galicia, ou Xeración Europea Federalista Galicia, é unha asociación sen ánimo de lucro adicada ao fomento da participación da xuventude galega na construción dunha Europa federal.',
   openGraph: {
     type: 'website',
     locale: 'gl',
