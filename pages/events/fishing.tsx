05243d82c531270ab71292a896354f7238582c21
Cristina Correa <cristina.correa.segade@gmail.com>
2023-05-15 21:03:05 +0200

    Evento pesca actualizado

--- pages/events/fishing.tsx
+++ pages/events/fishing.tsx
@@ -30,18 +30,27 @@ export default function Fishing() {
         <p className="mb-6">
           Se queres saber máis sobre este evento, podes contactarnos a través das nosas redes
           sociais, ou enviando un mail a{' '}
-          <a href="mailto:contact@jef.gal" className=" opacity-60">
-            contact@jef.gal
+          <a href="mailto:evento.pesca@jef.gal" className="opacity-60">
+            evento.pesca@jef.gal
           </a>
           .
         </p>
+        <p className='mb-6'><span className="font-semibold">Onde:</span> <br/>
+          Salón de Actos, Escola Técnica Superior de Enxeñaría <br/>
+          Rúa de Lope Gómez de Marzoa, S/N <br/>
+          Campus Vida, Universidade de Santiago de Compostela <br/>
+          15705 Santiago de Compostela <br />
+          <a href="https://goo.gl/maps/vBw1zKRBJreEp8b98" className="opacity-60">📍Tes aquí a direción do Google Maps</a>
+        </p>
         <p className="mb-6">Esperámoste! 🙌</p>
-        <a
-          href="https://www.eventbrite.es/e/el-sector-pesquero-gallego-ante-la-restriccion-de-la-pesca-de-fondo-tickets-620268839797"
-          className="w-full text-sm px-4 py-2 leading-none border rounded transition text-black border-black dark:border-white dark:hover:border-transparent dark:text-white border-opacity-30 hover:border-transparent hover:text-white hover:bg-primary mt-4"
-        >
-          {'Reserva o teu sitio! 🪑'}
-        </a>
+        <div className="justify-center">
+          <a
+            href="https://www.eventbrite.es/e/el-sector-pesquero-gallego-ante-la-restriccion-de-la-pesca-de-fondo-tickets-620268839797"
+            className="w-auto font-semibold text-sm px-4 py-2 leading-none border rounded transition text-black border-black dark:border-white dark:hover:border-transparent dark:text-white border-opacity-30 hover:border-transparent hover:text-white hover:bg-primary mt-4"
+          >
+            {'Reserva o teu sitio! 🪑'}
+          </a>
+        </div>
       </BoxComponent>
     </>
   );
