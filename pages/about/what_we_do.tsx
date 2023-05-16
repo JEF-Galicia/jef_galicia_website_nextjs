4bd29a8e421e5e4ba5538dd44c9b5571fa8889d2
ACMCMC <20495460+ACMCMC@users.noreply.github.com>
2023-05-16 15:17:10 +0200

    design

--- pages/about/what_we_do.tsx
+++ pages/about/what_we_do.tsx
@@ -1,5 +1,10 @@
 import { NextSeo } from "next-seo";
 import BoxComponent from "../../components/Box";
+import Image from "next/image";
+import image1 from '../../public/Congress_EU_Year_Youth.jpg';
+import image2 from '../../public/Speaking_In_Library.jpg';
+import image3 from '../../public/Italy_Congress.jpg';
+import image4 from '../../public/EU_Handshake.jpg';
 
 export default function WhatWeDo() {
     return (
@@ -10,7 +15,35 @@ export default function WhatWeDo() {
             />
             <BoxComponent>
                 <h1 className='text-3xl font-semibold my-6'>Que facemos</h1>
-                <p className='mb-6'>A nosa labor concretízase no fomento da participación da xuventude galega na construción dunha Europa máis unida e democrática, a través da organización de actividades e campañas de sensibilización e formación. Ademais, JEF Galicia tamén colabora con outras organizacións e institucións galegas para promover a participación cidadá e o compromiso político entre a xuventude galega.</p>
+                <div className="h-22 flex mx-0 mb-4 flex-col place-items-center rounded">
+                    <Image src={image1} alt="Congreso do EU Year Of Youth" className="rounded" />
+                </div>
+                <h3 className="text-2xl font-semibold mt-6 mb-2">Actividades</h3>
+                <p className="mb-6">
+                    A nosa asociación organiza eventos e actividades para os nosos
+                    membros e para o público en xeral. Estas actividades poden ser
+                    de todo tipo, desde debates e conferencias, ata eventos de
+                    networking e actividades de grupo.
+                </p>
+                <div className="h-22 flex mx-0 mt-10 mb-4 flex-col place-items-center rounded">
+                    <Image src={image2} alt="Persoas mozas falando nunha biblioteca" className="rounded" />
+                </div>
+                <h3 className="text-2xl font-semibold mt-6 mb-2">Iniciativas</h3>
+                <p className="mb-6">
+                </p>
+                <div className="h-22 flex mx-0 mt-10 mb-4 flex-col place-items-center rounded">
+                    <Image src={image3} alt="Muller falando nun congreso en Italia" className="rounded" />
+                </div>
+                <h3 className="text-2xl font-semibold mt-6 mb-2">Congresos</h3>
+                <p className="mb-6">
+                </p>
+                <div className="h-22 flex mx-0 mt-10 mb-4 flex-col place-items-center rounded">
+                    <Image src={image4} alt="Moza xove dando a man a unha política" className="rounded" />
+                </div>
+                <h3 className="text-2xl font-semibold mt-6 mb-2">Proxectos</h3>
+                <p className="mb-6">
+
+                </p>
             </BoxComponent>
         </>
     )
