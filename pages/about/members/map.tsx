0169161fb64695995941fd00bef6e5db1d4adad3
ACMCMC <20495460+ACMCMC@users.noreply.github.com>
2024-08-11 09:42:00 +0000

    Bump up version

--- pages/about/members/map.tsx
+++ pages/about/members/map.tsx
@@ -146,21 +146,27 @@ export default function Index({ users, photos, memberships, groups, places }: In
                 <Map height={500} defaultZoom={4} boxClassname='' attribution={false} defaultCenter={[50.0, 5.0]}>
                     {usersWithAddressesOrdered.map((user) => {
                         const tagline = (user.addresses as [{ type: string, formatted: string }])?.find(a => a.type === 'work')?.formatted;
-                        return (
-                            <Marker key={user.id} width={50} anchor={[places[tagline]?.places[0].location?.latitude, places[tagline]?.places[0].location?.longitude]} >
-                                <svg width={40} height={50} viewBox="0 0 61 71" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <g style={{ pointerEvents: 'auto' }}>
-                                        <path
-                                            d="M52 31.5C52 36.8395 49.18 42.314 45.0107 47.6094C40.8672 52.872 35.619 57.678 31.1763 61.6922C30.7916 62.0398 30.2084 62.0398 29.8237 61.6922C25.381 57.678 20.1328 52.872 15.9893 47.6094C11.82 42.314 9 36.8395 9 31.5C9 18.5709 18.6801 9 30.5 9C42.3199 9 52 18.5709 52 31.5Z"
-                                            fill={"var(--color-primary)"}
-                                            stroke="white"
-                                            strokeWidth="4"
-                                        />
-                                        <circle cx="30.5" cy="30.5" r="8.5" fill="white" opacity={0.6} />
-                                    </g>
-                                </svg>
-                            </Marker>
-                        )
+                        try {
+                            console.log(places[tagline]);
+                            return (
+                                <Marker key={user.id} width={50} anchor={[places[tagline]?.places[0].location?.latitude, places[tagline]?.places[0].location?.longitude]} >
+                                    <svg width={40} height={50} viewBox="0 0 61 71" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <g style={{ pointerEvents: 'auto' }}>
+                                            <path
+                                                d="M52 31.5C52 36.8395 49.18 42.314 45.0107 47.6094C40.8672 52.872 35.619 57.678 31.1763 61.6922C30.7916 62.0398 30.2084 62.0398 29.8237 61.6922C25.381 57.678 20.1328 52.872 15.9893 47.6094C11.82 42.314 9 36.8395 9 31.5C9 18.5709 18.6801 9 30.5 9C42.3199 9 52 18.5709 52 31.5Z"
+                                                fill={"var(--color-primary)"}
+                                                stroke="white"
+                                                strokeWidth="4"
+                                            />
+                                            <circle cx="30.5" cy="30.5" r="8.5" fill="white" opacity={0.6} />
+                                        </g>
+                                    </svg>
+                                </Marker>
+                            )
+                        } catch (e) {
+                            console.error(e);
+                            return <></>
+                        }
                     })}
                 </Map>
             </div>
